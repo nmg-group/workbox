657414bad2299823748329f239dfbcae6e6f5f27
nmendezgranton <nmendez@nmg.systems>
2020-06-15 15:12:35 -0300

    Carpeta resources not found

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-bash resources/00.init.sh
-bash resources/01.lamp-install.sh
-bash resources/02.nas.sh
-bash resources/03.proxy.sh
-bash resources/04.vpn.sh
-bash resources/04.01.vpn-clientes.sh
-bash resources/99.post-install.sh
+bash 00.init.sh
+bash 01.lamp-install.sh
+bash 02.nas.sh
+bash 03.proxy.sh
+bash 04.vpn.sh
+bash 04.01.vpn-clientes.sh
+bash 99.post-install.sh
 reboot
