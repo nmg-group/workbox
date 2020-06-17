9f5a81368b5fbe1193f2c8d841d8b3dd20fb5431
nmendezgranton <nmendez@nmg.systems>
2020-06-17 19:43:22 -0300

    Postfix headless

--- 00.init.sh
+++ 00.init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Setup defaults para postfix (headless install)
+debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
 apt-get --assume-yes install zip unzip mailutils
